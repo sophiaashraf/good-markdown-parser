c9523b03820132f0a9d525f7d02abdecf6441028
sophiaashraf <sashraf@ucsd.edu>
2022-05-26 14:15:11 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,6 +13,7 @@ public class MarkdownParseTest {
         assertEquals(4,4);
     }
 
+    //should return [google.com]
     @Test
     public void TestSnip1() throws IOException{
         List expected = List.of("google.com");
@@ -22,6 +23,34 @@ public class MarkdownParseTest {
         ArrayList<String> actual = MarkdownParse.getLinks(content);
 
         System.out.println("Snippet 1: " + actual);
+
+        assertEquals(expected, actual);
+    }
+
+    //should return [a.com], [a.com(())], [example.com]
+    @Test
+    public void TestSnip2() throws IOException{
+        List expected = List.of("a.com", "a.com(())","example.com");
+
+        Path fileName = Path.of("test-file10.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+
+        System.out.println("Snippet 2: " + actual);
+
+        assertEquals(expected, actual);
+    }
+
+    //should return [twitter.com], [https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule]
+    @Test
+    public void TestSnip3() throws IOException{
+        List expected = List.of("twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+
+        Path fileName = Path.of("test-file11.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+
+        System.out.println("Snippet 3: " + actual);
 
         assertEquals(expected, actual);
     }
